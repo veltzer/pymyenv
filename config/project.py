3b41b4ac57c0784f583f1481e4dac11ca50c336b
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-27 00:33:50 +0300

    better project config

--- config/project.py
+++ config/project.py
@@ -23,7 +23,7 @@ project_keywords = [
     "virtualenv",
 ]
 project_license = "MIT"
-project_year_started = "2017"
+project_year_started = 2017
 project_description = project_short_description
 project_platforms = [
     "python3",
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
         'pylogconf',
     ],
     extras_require={
-    },
+    }},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
