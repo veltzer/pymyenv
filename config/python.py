22f09f1c9579769db34fc465ee58321a1aac233a
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-07 14:27:37 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -5,12 +5,17 @@ package_name = config.project.project_name
 console_scripts = [
     "pymyenv=pymyenv.main:main",
 ]
-
+dev_requires = [
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
+    "Sphinx",
+    "pymakehelper",
+]
 install_requires = [
     "pytconf",
     "pylogconf",
 ]
-
 test_requires = [
     "pylint",
     "pytest",
@@ -19,14 +24,7 @@ test_requires = [
     "flake8",
 ]
 
-dev_requires = [
-    "pyclassifiers",
-    "pypitools",
-    "pydmt",
-    "Sphinx",
-    "pymakehelper",
-]
+python_requires = ">=3.10"
 
-python_requires = ">=3.9"
-test_os = ["ubuntu-20.04"]
-test_python = ["3.9"]
+test_os = ["ubuntu-22.04"]
+test_python = ["3.10"]
