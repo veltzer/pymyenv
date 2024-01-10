5b20a837c8fdbd5512684a06505823bbb967264b
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-10 18:18:01 +0200

    fixing reqs

--- config/python.py
+++ config/python.py
@@ -4,20 +4,20 @@ from typing import List
 console_scripts: List[str] = [
     "pymyenv=pymyenv.main:main",
 ]
+dev_requires: List[str] = [
+    "pypitools",
+]
 config_requires: List[str] = [
     "pyclassifiers",
 ]
-dev_requires: List[str] = [
-    "pypitools",
+install_requires: List[str] = [
+    "pytconf",
+    "pylogconf",
 ]
 make_requires: List[str] = [
     "pyclassifiers",
     "pymakehelper",
     "pydmt",
-]
-install_requires: List[str] = [
-    "pytconf",
-    "pylogconf",
 ]
 test_requires: List[str] = [
     "pylint",
